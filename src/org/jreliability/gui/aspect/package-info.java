05de8e2997973afc433ce5ab50c9348d2941ef60
Michael Glaß <michael.glass@uni-ulm.de>
2008-10-29 13:13:17 +0000

    Added missing package-infos.
    git-svn-id: svn://svn.code.sf.net/p/jreliability/code/jreliability/trunk@102 0b33cc7e-343a-ee05-a097-9295465598fc

---
+++ src/org/jreliability/gui/aspect/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes for the different aspects of the functions and their plots in the GUI.
+ * 
+ */
+package org.jreliability.gui.aspect;
---
+++ src/org/jreliability/gui/sampler/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes for the samples derived by evaluators and their plots/histograms in the GUI.
+ * 
+ */
+package org.jreliability.gui.sampler;
