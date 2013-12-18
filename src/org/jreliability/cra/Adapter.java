e71f53fbe1f699cfa31615eb1c85d89d32a6e7b9
Felix Reimann <felix@fex-it.de>
2013-12-18 10:32:00 +0000

    javadoc
    git-svn-id: svn://svn.code.sf.net/p/jreliability/code/jreliability/branches/cra@165 0b33cc7e-343a-ee05-a097-9295465598fc

--- src/org/jreliability/cra/Adapter.java
+++ src/org/jreliability/cra/Adapter.java
@@ -1,5 +1,22 @@
 package org.jreliability.cra;
 
+/**
+ * The {@link Adapter} takes exactly one input model of type {@code I} and
+ * transforms it to an output model of type {@code O}.
+ * 
+ * @author reimann
+ * 
+ * @param <I>
+ *            input type
+ * @param <O>
+ *            output type
+ */
 public interface Adapter<I, O> extends Node {
+	/**
+	 * Transform the input to the output.
+	 * 
+	 * @param i
+	 * @return
+	 */
 	public O transform(I i);
 }
