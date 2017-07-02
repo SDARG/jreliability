1afe9e440bd08bc6b3b41d46244a62aecabbb0d5
Michael Glass <michael.glass@uni-ulm.de>
2017-07-02 21:03:28 +0200

    test for simple function transformer

---
+++ src/test/java/org/jreliability/function/common/SimpleFunctionTransformerTest.java
@@ -0,0 +1,57 @@
+/**
+ * JReliability is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * JReliability is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Opt4J. If not, see http://www.gnu.org/licenses/.
+ */
+package org.jreliability.function.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jreliability.function.ReliabilityFunction;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * The {@link SimpleFunctionTransformerTest} to test the
+ * {@link SimpleFunctionTransformer}.
+ * 
+ * @author glass
+ *
+ */
+public class SimpleFunctionTransformerTest {
+
+	@Test
+	public void testSet() {
+		SimpleFunctionTransformer<String> transformer = new SimpleFunctionTransformer<String>();
+		String string = "EVENT";
+		ReliabilityFunction f = new ExponentialReliabilityFunction(0.005);
+		transformer.set(string, f);
+		Assert.assertSame(f, transformer.transform(string));
+	}
+	
+	@Test
+	public void testTransform() {
+		Map<String, ReliabilityFunction> map = new HashMap<String, ReliabilityFunction>();
+		String string = "EVENT";
+		ReliabilityFunction f = new ExponentialReliabilityFunction(0.005);
+		map.put(string, f);
+		String stringTwo = "EVENT2";
+		ReliabilityFunction fTwo = new WeibullReliabilityFunction(0.004,2);
+		map.put(stringTwo, fTwo);
+		
+		SimpleFunctionTransformer<String> transformer = new SimpleFunctionTransformer<String>(map);
+		Assert.assertSame(f, transformer.transform(string));
+		Assert.assertSame(fTwo, transformer.transform(stringTwo));
+	}
+
+}
