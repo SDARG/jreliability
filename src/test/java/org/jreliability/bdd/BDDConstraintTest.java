cabf5750808f3f14f4a11f88ff08e19efb44a8ff
Michael Glass <michael.glass@uni-ulm.de>
2017-07-04 21:03:50 +0200

    comments and license

--- src/test/java/org/jreliability/bdd/BDDConstraintTest.java
+++ src/test/java/org/jreliability/bdd/BDDConstraintTest.java
@@ -1,3 +1,15 @@
+/**
+ * JReliability is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * JReliability is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with Opt4J. If not, see
+ * http://www.gnu.org/licenses/.
+ */
 package org.jreliability.bdd;
 
 import org.jreliability.bdd.BDDConstraint.Literal;
@@ -7,6 +19,12 @@ import org.jreliability.bdd.javabdd.JBDDProviderFactory.Type;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * The {@code BDDConstraintTest} tests the {@code BDDConstraint}.
+ * 
+ * @author reimann
+ *
+ */
 public class BDDConstraintTest {
 
 	@Test
